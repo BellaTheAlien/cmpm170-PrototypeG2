f3048d120bdc898724e6579241de160a1bb71d05
BellaTheAlien <161175405+BellaTheAlien@users.noreply.github.com>
2025-10-22 14:14:49 -0700

    added the player to flip on x-axes when moving

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -4,16 +4,25 @@ public class player_movement : MonoBehaviour
 {
     [SerializeField]private float speed;
     private Rigidbody2D body;
+    private Vector3 initialScale;
 
     private void Awake() {
         body = GetComponent<Rigidbody2D>();
+        initialScale = transform.localScale;
     }
 
     private void Update() {
         float horizontalInput = Input.GetAxis("Horizontal");
-        //float verticalInput = Input.GetAxis("Vertical");
 
         body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
+
+        if(horizontalInput > 0.01f) {
+            transform.localScale = initialScale;
+        }
+        else if(horizontalInput < -0.01f) {
+            // Flip the player sprite
+            transform.localScale = new Vector3(-initialScale.x, initialScale.y, initialScale.z);
+        }
 
         if(Input.GetKeyDown(KeyCode.Space)) {
             body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
